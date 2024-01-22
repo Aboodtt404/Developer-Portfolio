b497a90d79d0f126d3c039da1f57d3698e2ae49c
Abdelrahman Emad <abdelrahmansat2003@gmail.com>
2024-01-22 23:05:06 +0300

    finished island / contact page done

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,65 @@
+import { Link } from "react-router-dom";
+
+import { arrow } from "../assets/icons";
+
+const HomeInfo = ({ currentStage }) => {
+  if (currentStage === 1)
+    return (
+      <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+        Hi, I'm
+        <span className='font-semibold mx-2 text-white'>Abdelrahman</span>
+        👋
+        <br />
+        An AI Engineer from Egypt
+      </h1>
+    );
+
+  if (currentStage === 2) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium sm:text-xl text-center'>
+        I freelanced on Fiverr <br /> showcasing my skills as a versatile professional in web development, game development, and Photoshop
+        </p>
+
+        <Link to='/about' className='neo-brutalism-white neo-btn'>
+          Learn more
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 3) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium text-center sm:text-xl'>
+        Throughout my college years, I've consistently found joy in <br /> working on various projects, reveling in the creative and learning experiences they offer (yes, I'm still in college)
+        </p>
+
+        <Link to='/projects' className='neo-brutalism-white neo-btn'>
+          Visit my portfolio
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 4) {
+    return (
+      <div className='info-box'>
+      <p className='font-medium sm:text-xl text-center'>
+        Need a project done or looking for a dev? <br/> I'm literally just a few keystrokes away
+      </p>
+
+      <Link to='/contact' className='neo-brutalism-white neo-btn'>
+        Let's talk
+        <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+      </Link>
+    </div>
+    );
+  }
+
+  return null;
+};
+
+export default HomeInfo;
---
+++ src/models/index.js
@@ -0,0 +1,7 @@
+import {Bird} from './Bird'
+import {Island} from './Island';
+import {Plane} from './Plane'
+import Sky from './Sky'
+
+
+export { Bird, Island, Plane, Sky }
